ee2ab1d10ca1e3fa43f5e71021b5afb09dd7dfb9
HeroBuxx <me@herobuxx.xyz>
2021-03-05 12:36:58 +0000

    SETUP: Add restriction on bashrc
    Signed-off-by: HeroBuxx <me@herobuxx.xyz>

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ echo "SETUP: Installing some mandatory scripts..."
 sudo install bin/lsreg /bin/lsreg
 sudo install bin/lsdel /bin/lsdel
 sudo install bin/user /bin/user
+sudo install bin/bashrc bin/bashrc
 
 # Create usegroup
 sudo groupadd spci
