828d3a30f7d4df5c82159e3e6527c5a16468ede9
HeroBuxx <me@herobuxx.xyz>
2021-03-05 12:23:57 +0000

    SETUP: Initialize script
    Signed-off-by: HeroBuxx <me@herobuxx.xyz>

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copyright (C) 2021 The Spacheship Project behalf with Lunati1cSpace
+# Setup script for Spaceship CI
+
+# Install script for setuping user
+echo "SETUP: Installing some mandatory scripts..."
+sudo install bin/lsreg /bin/lsreg
+sudo install bin/lsdel /bin/lsdel
+sudo install bin/user /bin/user
+
+# Create usegroup
+sudo groupadd spci
